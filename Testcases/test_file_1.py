781cc68ee2291e12a51e0f5ad4c4381072fbebe9
komalgore102 <abhishelar97@gmail.com>
2023-08-10 22:42:40 +0530

    first commit

---
+++ Testcases/test_file_1.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+class Test_001:
+
+    def test_sum_001(self):
+        a=10
+        b=30
+        sum=a+b
+        print(sum)
+        if sum == 10:
+            assert True
+        else:
+            assert False
+
+    def test_login_002(self):
+        driver=webdriver.Chrome()
+        driver.get("https://credence.in/")
+        if driver.title == "Credence":
+            print("Welcome....You are at credence ")
+            assert True
+        else:
+            print("Sorry not found")
+            assert False
